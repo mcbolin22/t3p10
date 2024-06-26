2492a508fce994d39140bc5a41634eab62e276a3
Colin <mcbolin@hotmail.com>
2024-06-26 15:44:47 +1000

    feat: add Increase/Decrease number in state

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+// number of cards to render = 6
+// use state to store data with classes
+// do not use "let numberOfCardsToRender = 6;"
+// use the following to store data:
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = 
+    {
+      numberOfPokemonToShow: 6
+    }
+
+    this.increaseNumber = this.increaseNumber.bind(this);
+    this.decreaseNumber = this.decreaseNumber.bind(this);
+  }
+
+  exampleFunction(){
+    console.log("Example function was run!")
+  }
+
+  increaseNumber(){
+    this.setState({numberOfPokemonToShow: this.state.numberOfPokemonToShow + 1});
+     
+  }
+
+  decreaseNumber(){
+    this.setState({numberOfPokemonToShow: this.state.numberOfPokemonToShow - 1});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Number of Pokemon to show: {this.state.numberOfPokemonToShow}</h1>
+        <button onClick={this.increaseNumber}>
+          Increase number
+        </button>
+        <button onClick={this.decreaseNumber}>
+          Decrease number
+        </button>
+      </div>
+    );
+
+  }
+  
+}
+
+export default App;
